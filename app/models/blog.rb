a2c208553ee7eb5e1a238774cccf016565756640
gpessacq <gustavo@tigerstrip.es>
2016-09-26 12:44:18 -0300

    Add user_id to blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920194946) do
+ActiveRecord::Schema.define(version: 20160926154053) do
 
   create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "password_resets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
